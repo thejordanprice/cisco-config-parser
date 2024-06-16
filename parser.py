e37b2cc7e1352f825b4aa9fb1da8ebc65a97c8af
Jordan Price <thejordanprice@gmx.com>
2024-06-15 22:37:49 -0400

    Update parser.py
    added bang detection for blocks

--- parser.py
+++ parser.py
@@ -5,6 +5,8 @@ import json
 def parse_cisco_config(filename):
     config = {}
     current_interface = None
+    inside_interface_block = False
+    
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -14,6 +16,7 @@ def parse_cisco_config(filename):
         if re.match(r'^hostname\s+(.+)$', line):
             match = re.match(r'^hostname\s+(.+)$', line)
             config['hostname'] = match.group(1)
+            current_interface = None
         
         elif re.match(r'^interface\s+(.+)$', line):
             match = re.match(r'^interface\s+(.+)$', line)
@@ -22,6 +25,12 @@ def parse_cisco_config(filename):
                 config['interfaces'] = {}
             if current_interface not in config['interfaces']:
                 config['interfaces'][current_interface] = {}
+            inside_interface_block = True
+        
+        elif line == '!':
+            if inside_interface_block:
+                current_interface = None
+                inside_interface_block = False
         
         elif current_interface:
             match = re.match(r'^\s*(\S+)\s+(.+)$', line)
@@ -29,6 +38,7 @@ def parse_cisco_config(filename):
                 key = match.group(1)
                 value = match.group(2)
                 config['interfaces'][current_interface][key] = value
+        
         else:
             # Handle standalone configurations
             standalone_match = re.match(r'^(no aaa new-model|switch \d+ provision .+|mls qos .+|spanning-tree .+|auto qos .+|vlan .+)$', line)
